a08d7696195969363f7b809148835b022aac7a1c
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-22 08:52:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let body = document.getElementsByTagName("body")[0];
+let p = document.createElement('p');
+p.textContent = "DOM load success";
+body.appendChild(p);
